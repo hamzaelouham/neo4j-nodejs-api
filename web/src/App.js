eee6a7eafb048c0e4a950011976fdec33d3c228f
hamzaeelouham <hamzaelouham@gmail.com>
2023-02-02 10:23:46 +0000

    sf

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,15 @@
+import React from "react";
 import Nav from "./components/Nav";
 import Search from "./components/Search";
 
 function App() {
   const [search, setSearch] = React.useState("");
+  const url = `http://localhost:5000/query/${search}`;
   const SearchHandler = async (e) => {
     e.preventDefault();
     try {
-      const result = await (await fetch("url")).json();
+      const result = await (await fetch(url)).json();
+      console.log(result);
     } catch (error) {}
   };
 

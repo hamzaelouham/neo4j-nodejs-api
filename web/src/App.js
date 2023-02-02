666be438df3ba3ac8d958a1114fb7416e200b45f
hamzaeelouham <hamzaelouham@gmail.com>
2023-02-02 01:24:37 +0000

    web

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,13 @@
+import Nav from "./components/Nav";
+import Search from "./components/Search";
+
 function App() {
-  return <div className="h-screen"></div>;
+  return (
+    <div className="h-screen bg-gray-100">
+      <Nav />
+      <Search />
+    </div>
+  );
 }
 
 export default App;
---
+++ web/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="navbar navbar-expand-lg  py-2 bg-white relative flex items-center w-full justify-between h-9"></div>
+  );
+}
